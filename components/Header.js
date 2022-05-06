83c33e25da90b1a59e22439a9aad6c0c9a9ec193
Suchit Deshmukh <56268987+suchitd11@users.noreply.github.com>
2022-05-06 14:21:45 +0530

    Navbar

---
+++ components/Header.js
@@ -0,0 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react' 
+import Image from 'next/image'
+import { 
+    SearchIcon, 
+    HomeIcon,
+    PlusIcon,
+    StarIcon
+} from "@heroicons/react/solid"
+
+function Header() {
+  return (
+    <div className='sticky bg-[#040714] top-0 z-[1000] flex items-center px-10 md:px-12 h-[72px]'>
+    <Image className='cursor-pointer' src="/images/logo.svg" width={80} height={80} alt="logo"/>
+    <div className='hidden ml-10 md:flex items-center space-x-6'>
+    <a className='header-link group'>
+            <HomeIcon className='h-4'/>
+            <span className='span'>Home</span>
+        </a>
+        <a className='header-link group'>
+            <SearchIcon className='h-4'/>
+            <span className='span'>Search</span>
+        </a>
+        <a className='header-link group'>
+            <PlusIcon className='h-4'/>
+            <span className='span'>Watchlist</span>
+        </a>
+        <a className='header-link group'>
+            <StarIcon className='h-4'/>
+            <span className='span'>Originals</span>
+        </a>
+        <a className="header-link group">
+            <img src="/images/movie-icon.svg" alt="" className="h-5" />
+            <span className="span">Movies</span>
+        </a>
+        <a className="header-link group">
+            <img src="/images/series-icon.svg" alt="" className="h-5" />
+            <span className="span">Series</span>
+        </a>
+    </div>
+    <button>
+        
+    </button>
+    </div>
+  )
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "../styles.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
